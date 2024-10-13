dfdca2a14f525a3c69ff5ddf99f0196d529841aa
kavispires <kavispires@gmail.com>
2024-10-13 16:18:50 -0700

    Added logo to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
+import { Content } from 'components/Content';
+
 export function HomePage() {
   return (
-    <div className="w-full grid place-items-center">
-      <h1>LD</h1>
-    </div>
+    <Content>
+      <div className="w-full  grid place-items-center">
+        <img
+          src={`${process.env.PUBLIC_URL}/images/logo.svg`}
+          alt="LD logo"
+          style={{ maxWidth: '30vw', minWidth: '300px' }}
+        />
+      </div>
+    </Content>
   );
 }
