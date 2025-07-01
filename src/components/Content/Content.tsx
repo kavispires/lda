96337aee6223d5acb075e48e764bf2f420f17d87
kavispires <kavispires@gmail.com>
2025-07-01 11:30:50 -0700

    Updated home page

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -4,11 +4,11 @@ import { forwardRef, type ReactNode } from 'react';
 type ContentProps = {
   children: ReactNode;
   className?: string;
-};
+} & React.HTMLAttributes<HTMLElement>;
 
-export const Content = forwardRef<HTMLElement, ContentProps>(({ children, className }, ref) => {
+export const Content = forwardRef<HTMLElement, ContentProps>(({ children, className, ...props }, ref) => {
   return (
-    <main ref={ref} className={clsx('m-4', className)}>
+    <main ref={ref} className={clsx('m-4', className)} {...props}>
       {children}
     </main>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,19 @@ import { Content } from 'components/Content';
 
 export function HomePage() {
   return (
-    <Content>
-      <div className="w-full  grid place-items-center">
-        <img src={'images/logo.svg'} alt="LD logo" style={{ maxWidth: '30vw', minWidth: '300px' }} />
+    <Content style={{ margin: 0 }}>
+      <div className="relative w-full h-full min-h-screen">
+        {/* Background image with full coverage */}
+        <img
+          src="images/background.jpg"
+          alt="LD background"
+          className="absolute inset-0 w-full h-full object-cover z-0"
+        />
+
+        {/* Logo centered on top of background */}
+        <div className="absolute z-10 w-full h-full flex items-center justify-center">
+          <img src="images/logo.svg" alt="LD logo" className="w-[30vw] min-w-[150px] max-w-[300px]" />
+        </div>
       </div>
     </Content>
   );
