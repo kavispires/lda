34e19db488764a52bcf7a34c8956bae9b2f6522c
kavispires <kavispires@gmail.com>
2025-12-04 19:00:54 -0800

    Added image to home page

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -4,11 +4,11 @@ import { forwardRef, type ReactNode } from 'react';
 type ContentProps = {
   children: ReactNode;
   className?: string;
-};
+} & React.HTMLAttributes<HTMLElement>;
 
-export const Content = forwardRef<HTMLElement, ContentProps>(({ children, className }, ref) => {
+export const Content = forwardRef<HTMLElement, ContentProps>(({ children, className, ...props }, ref) => {
   return (
-    <main className={clsx('m-4', className)} ref={ref}>
+    <main className={clsx('m-4', className)} ref={ref} {...props}>
       {children}
     </main>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,22 @@ import { Content } from 'components/Content';
 
 export function HomePage() {
   return (
-    <Content>
-      <div className="w-full  grid place-items-center">
-        <img alt="LD logo" src={'images/logo.svg'} style={{ maxWidth: '30vw', minWidth: '300px' }} />
+    <Content
+      className="w-full grid place-items-center"
+      style={{
+        height: '100%',
+        backgroundImage: 'url(images/background.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        margin: '0',
+      }}
+    >
+      <div className="w-full grid place-items-center">
+        <img
+          alt="LD logo"
+          src={'images/logo.svg'}
+          style={{ maxWidth: '30vw', minWidth: '300px', opacity: 0.8 }}
+        />
       </div>
     </Content>
   );
