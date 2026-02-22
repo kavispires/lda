67e11d6c1889e65e8d3e5a387147df649d605bdd
kavispires <kavispires@gmail.com>
2026-02-22 14:40:53 -0800

    Updated Distributions listing page with Firestore links

--- src/pages/Distributions/DistributionsListingPage.tsx
+++ src/pages/Distributions/DistributionsListingPage.tsx
@@ -1,12 +1,13 @@
-import { ArrowRightOutlined, DeleteFilled } from '@ant-design/icons';
-import { Button, Popconfirm, Space, Table, Typography } from 'antd';
+import { BarChartOutlined, DeleteFilled, FormOutlined } from '@ant-design/icons';
+import { Button, Divider, Flex, Popconfirm, Space, Table, type TableProps, Typography } from 'antd';
+import { FirestoreConsoleLink } from 'components/Common/FirestoreConsoleLink';
 import { Content, ContentError, ContentLoading } from 'components/Content';
 import { ListingSelect, useListingSelect } from 'components/Listing/ListingSelect';
 import { useDeleteDistributionMutation } from 'hooks/useDistribution';
 import { useListingQuery } from 'hooks/useListingQuery';
 import { useTablePagination } from 'hooks/useTablePagination';
-import { Link, useNavigate } from 'react-router-dom';
-import type { DistributionListingData, ListingEntry } from 'types';
+import { useNavigate } from 'react-router-dom';
+import type { DistributionListingData, ListingEntry, UID } from 'types';
 
 const ALL_GROUPS = 'All Groups';
 
@@ -27,42 +28,33 @@ export function DistributionsListingPage() {
     return <ContentError>{distributionsQuery.error.message}</ContentError>;
   }
 
-  const columns = [
+  const columns: TableProps<ListingEntry>['columns'] = [
     {
       title: 'Name',
       dataIndex: 'name',
       key: 'name',
-      render: (name: string, record: ListingEntry) => (
-        <Link to={`/distributions/${record.id}`}>
-          {name} <ArrowRightOutlined />
-        </Link>
-      ),
+      sorter: (a: ListingEntry, b: ListingEntry) => a.name.localeCompare(b.name),
     },
-    {
-      title: 'Type',
-      dataIndex: 'type',
-      key: 'type',
-    },
-    {
-      title: 'Id',
-      dataIndex: 'id',
-      'key:': 'id',
-    },
+
     {
       title: 'Actions',
       key: 'actions',
       render: (record: ListingEntry) => (
-        <Space size="middle">
-          <Button onClick={() => navigate(`/distributions/${record.id}/edit`)} type="default">
+        <Space separator={<Divider orientation="vertical" />} size="small">
+          <Button
+            icon={<BarChartOutlined />}
+            onClick={() => navigate(`/distributions/${record.id}`)}
+            type="link"
+          >
+            View
+          </Button>
+          <Button
+            icon={<FormOutlined />}
+            onClick={() => navigate(`/distributions/${record.id}/edit`)}
+            type="link"
+          >
             Edit
           </Button>
-
-          <Popconfirm
-            onConfirm={() => deleteDistributionMutation.mutate(record.id)}
-            title="Are you sure you want to delete this distribution?"
-          >
-            <Button danger icon={<DeleteFilled />} loading={deleteDistributionMutation.isPending} />
-          </Popconfirm>
         </Space>
       ),
     },
@@ -74,6 +66,8 @@ export function DistributionsListingPage() {
             <Button
               icon={<i className="fi fi-rr-map-marker-edit"></i>}
               onClick={() => navigate(`/distributions/${record.id}/formation/${record?.data?.formationId}`)}
+              size="small"
+              type="link"
             >
               Edit
             </Button>
@@ -81,10 +75,41 @@ export function DistributionsListingPage() {
             <Button
               icon={<i className="fi fi-rr-marker"></i>}
               onClick={() => navigate(`/distributions/${record.id}/formation/$draft`)}
+              size="small"
             >
               Create
             </Button>
           )}
+        </Space>
+      ),
+    },
+    {
+      title: 'id',
+      dataIndex: 'id',
+      key: 'id',
+      render: (id: UID) => <FirestoreConsoleLink label={id} path={`songs/${id}`} />,
+    },
+    {
+      title: 'Type',
+      dataIndex: 'type',
+      key: 'type',
+    },
+    {
+      title: 'More',
+      dataIndex: 'id',
+      render: (songId: UID) => (
+        <Space size="middle">
+          <Popconfirm
+            onConfirm={() => deleteDistributionMutation.mutate(songId)}
+            title="Are you sure you want to delete this distribution?"
+          >
+            <Button
+              danger
+              icon={<DeleteFilled />}
+              loading={deleteDistributionMutation.isPending}
+              size="small"
+            />
+          </Popconfirm>
         </Space>
       ),
     },
@@ -93,7 +118,13 @@ export function DistributionsListingPage() {
   return (
     <Content>
       <Typography.Title level={2}>Distributions</Typography.Title>
-      <ListingSelect allKey={ALL_GROUPS} className="mb-2" options={options} paramKey="group" />
+      <Flex align="center" justify="space-between">
+        <ListingSelect allKey={ALL_GROUPS} className="mb-2" options={options} paramKey="group" />
+        <Flex align="center" gap={6}>
+          <FirestoreConsoleLink label="Listing" path="listings/distributions" />
+          <FirestoreConsoleLink label="Distributions" path="distributions" />
+        </Flex>
+      </Flex>
       <Table
         columns={columns}
         dataSource={activeList}
